187994e13ebd9d44685a6407e74515cee68c67ab
Rahul <rvchaudhari1000@gmail.com>
2023-01-20 00:19:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function myfunction()
+            {
+                var un = document.forms["myform"]["uname"].value;
+                var pw = document.forms["myform"]["pass"].value;
+                if(un=="Admin" && pw=="1234")
+                {
+                    window.location.href="welcome.html";
+                }
+                else
+                {
+                    alert("invalid username and password");
+                }
+
+            } 
+            let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
